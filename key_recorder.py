08c4e48ae4776f123422d251b27e9b86b827e8b2
timcrose <timcrose09@gmail.com>
2024-11-25 15:02:56 -0500

    Fixed an issue where holding a key and then holding shift and then releasing the key would cause it to think a key was released before pressed but it was only the uppercase version.

--- key_recorder.py
+++ key_recorder.py
@@ -6,20 +6,58 @@ Created on Fri Nov 22 17:04:29 2024
 """
 import pynput as pnp
 import time
-import sys
 import pandas as pd
+import string
 
-def on_press(key):
+def get_key_name(key, lowercase_only=True):
     '''
+    convert a pynput key into a str representing the key.
+    
+    Parameters
+    ----------
     key:
         for function keys: enum 'Key'
         for letter keys: pynput.keyboard._win32.KeyCode
+        
+    lowercase_only: bool
+        True: only worry about lowercase letters
+        False: attempt to keep track of uppercase letters too as different from
+            lowercase letters.
+            
+    Returns
+    -------
+    name: str
+        String code for the key as recognized by pynput.
     '''
-    global key_log, keys_to_wait_for_release
-    if hasattr(key, 'name'):
-        name = key.name
-    else:
-        name = key.char
+    name = str(key).replace('Key.','')
+    if lowercase_only:
+        name = name.lower()
+    return name
+
+
+def on_press(key):
+    '''
+    Record when the key is pressed unless the termination key is pressed, in which case,
+    write all recorded data to a file and exit the program.
+    
+    Parameters
+    ----------
+    key:
+        for function keys: enum 'Key'
+        for letter keys: pynput.keyboard._win32.KeyCode
+        
+    Returns
+    -------
+    continue_operation: None or bool
+        If None, continue. If False, abort the program.
+        
+    Notes
+    -----
+    1. The timestamp will become relative to the timestamp of the first key press
+        in the recording.
+    '''
+    global key_log, key_is_pressed
+    name = get_key_name(key, lowercase_only=True)
     if name == 'esc':
         df = pd.DataFrame(key_log)
         first_recorded_timestamp = key_log[0]['key_down_timestamp']
@@ -27,35 +65,76 @@ def on_press(key):
         df['key_up_timestamp'] = df['key_up_timestamp'] - first_recorded_timestamp
         df['key'] = df['key'].str.lower()
         df.to_csv('recorded_keystrokes.csv', index=False)
-        sys.exit()
-    if name not in keys_to_wait_for_release:
-        keys_to_wait_for_release.add(name)
+        return False
+    # Holding down a key causes that key to register as many on_press events, but
+    # we only want to record the first one.
+    if not key_is_pressed[name]:
         key_log.append({'key': name, 'key_down_timestamp': time.perf_counter()})
+        key_is_pressed[name] = True
+        
 
 def on_release(key):
     '''
+    Record when the key is released unless it had not yet been pressed (which usually
+    signals an issue).
+    
+    Parameters
+    ----------
     key:
         for function keys: enum 'Key'
         for letter keys: pynput.keyboard._win32.KeyCode
+        
+    Returns
+    -------
+    None
     '''
-    global key_log, keys_to_wait_for_release
-    if hasattr(key, 'name'):
-        name = key.name
-    else:
-        name = key.char
-    if name not in keys_to_wait_for_release:
-        #error
-        print('key:', name, 'got up before down', 'len(key_log)', len(key_log))
-        return
-    keys_to_wait_for_release.remove(name)
+    global key_log, key_is_pressed
+    name = get_key_name(key, lowercase_only=True)
+    if not key_is_pressed[name]:
+        if not key_is_pressed[name.lower()]:
+            #error
+            print('key:', name, 'got up before down', 'len(key_log)', len(key_log))
+            return
+        else:
+            name = name.lower()
+    key_is_pressed[name] = False
     for i in list(range(len(key_log)))[::-1]:
         if key_log[i]['key'] == name and 'key_up_timestamp' not in key_log[i]:
             key_log[i]['key_up_timestamp'] = time.perf_counter()
             break
     
 
-# listen for a hotkey
-key_log = []
-keys_to_wait_for_release = set()
-with pnp.keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+def record_keys(start_delay=0):
+    '''
+    Record all keystrokes by recording each key's press and release times. Store
+    that data to a file.
+
+    Parameters
+    ----------
+    start_delay: number
+        The amount of time to sleep before starting the recording.
+
+    Returns
+    -------
+    None
+    '''
+    global key_log, key_is_pressed
+    key_log = []
+    key_is_pressed = dict()
+    for name in string.ascii_letters + string.digits + string.punctuation:
+        key_is_pressed[name] = False
+    for key in pnp.keyboard.Key:
+        key_is_pressed[get_key_name(key, lowercase_only=True)] = False
+    # This sleep is in case the user pressses a button such as enter or F5 to start the program.
+    time.sleep(start_delay)
+    # Start listening for key presses. Esc ends the listening and records data.
+    with pnp.keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+        
+        
+def main():
+    record_keys(start_delay=0.15)
+    
+
+if __name__ == '__main__':
+    main()
--- recorded_keystrokes_player.py
+++ recorded_keystrokes_player.py
@@ -7,92 +7,122 @@ Created on Fri Nov 22 17:39:09 2024
 
 import pandas as pd
 import time
-import sys, os
-sys.path.append(os.path.join(r'C:\Users\timcr\Documents\python_packages\swg_code'))
 import pydirectinput_tmr as pdi
 pdi.FAILSAFE = False
 
-def press_sequentially():
-    pass
 
-def press(keys, presses=1, start_delay=0.0, return_delay=0.0, key_down_delay=0):
+def press(key, start_delay=0, return_delay=0, key_down_delay=0):
     '''
+    press a key for a specified length of time.
+    
     Parameters
     ----------
-    keys: list of str or list of list of str or str
-        List of keys.
-        e.g.
-        ['shift', 'a']
-        list of list of keys: equivalent to press_sequentially
-        e.g.
-        [['shift', 'a'], ['ctrl', 'c']]
-        str
-        e.g.
-        'esc' presses escape, '1', presses 1
+    key: str
+        pynput string representation of a key.
+        
+    start_delay: number
+        How long in seconds to delay starting the presses.
+        
+    return_delay: number
+        How long in seconds to delay returning after the presses.
+        
+    key_down_delay: number
+        How long in seconds to hold down each press.
 
     Returns
     -------
     None
-
-    Purpose
-    -------
-    This function enables you to press any number of keys in combination. i.e. you can hold down alt, ctrl, shift and then press a key using this function.
+    
+    Notes
+    -----
+    1. start_delay and return_delay should normally be 0 as having them be > 0 
+        will mess with the timings in a record. But, in case they are needed, 
+        they are available.
     '''
     time.sleep(start_delay)
-    for _ in range(presses):
-        if keys is None or len(keys) == 0:
-            break
-        elif type(keys) is str:
-            if key_down_delay > 0:
-                pdi.keyDown_fast(keys)
-                time.sleep(key_down_delay)
-                pdi.keyUp_fast(keys)
-            else:
-                pdi.press_key_fast(keys)
-        elif type(keys[0]) is list:
-            press_sequentially(keys, key_down_delay=key_down_delay)
-        elif type(keys[0]) is str or type(keys[0]) is int:
-            if type(keys[0]) is int:
-                keys = list(map(str,keys))
-            for key in keys:
-                pdi.keyDown_fast(key)
-            time.sleep(key_down_delay)
-            for key in keys[::-1]:
-                pdi.keyUp_fast(key)
+    pdi.keyDown_fast(key)
+    time.sleep(key_down_delay)
+    pdi.keyUp_fast(key)
     time.sleep(return_delay)
     
     
 def create_event_dataframe(df):
-  """
-  Creates a new DataFrame with columns 'key', 'action', and 'timestamp' by combining key_down and key_up events.
+    '''
+    Creates a new DataFrame with columns 'key', 'action', and 'timestamp' by combining key_down and key_up events.
+    
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        The original DataFrame with columns 'key', 'key_down_timestamp', and 'key_up_timestamp'.
+    
+    Returns
+    -------
+    df_events: pandas.DataFrame
+        A new DataFrame with columns 'key', 'action', and 'timestamp'.
+    '''
+    # Create two DataFrames, one for key_down and one for key_up events
+    df_down = df[['key', 'key_down_timestamp']].rename(columns={'key_down_timestamp': 'timestamp'})
+    df_down['action'] = 'down'
+    
+    df_up = df[['key', 'key_up_timestamp']].rename(columns={'key_up_timestamp': 'timestamp'})
+    df_up['action'] = 'up'
+    
+    # Combine the DataFrames and sort by timestamp
+    df_events = pd.concat([df_down, df_up], ignore_index=True)
+    df_events = df_events.sort_values(by='timestamp')
+    df_events['time_delta'] = df_events['timestamp'].diff().shift(-1).fillna(0.0)
+    df_events = df_events.drop(columns=['timestamp'])
+    return df_events
 
-  Args:
-    df: The original DataFrame with columns 'key', 'key_down_timestamp', and 'key_up_timestamp'.
 
-  Returns:
-    A new DataFrame with columns 'key', 'action', and 'timestamp'.
-  """
+def play_recorded_keystrokes(recorded_keystrokes_fpath, verbosity=0, start_delay=0, return_delay=0):
+    '''
+    Read a .csv dataframe file that stores each key's key, key_down_timestamp, and key_up_timestamp columns
+    and transform it into a form such that then this function will use to replay the events as sequential
+    keystrokes according to the timing as they were pressed during the recording.
 
-  # Create two DataFrames, one for key_down and one for key_up events
-  df_down = df[['key', 'key_down_timestamp']].rename(columns={'key_down_timestamp': 'timestamp'})
-  df_down['action'] = 'down'
+    Parameters
+    ----------
+    recorded_keystrokes_fpath: str or path
+        File path to .csv file that contains columns 'key', 'key_down_timestamp', and 'key_up_timestamp'.
+        This is the recorded keystrokes you want to replay.
+        
+    verbosity: int
+        0: no prints
+        1: final dataframe head
+        2: final and original dataframe heads
+        3: Print full final dataframe and original head.
+        4: Print full final dataframe and full orignal dataframe
+        
+    start_delay: number
+        How long in seconds to delay starting the presses.
+        
+    return_delay: number
+        How long in seconds to delay returning after the presses.
 
-  df_up = df[['key', 'key_up_timestamp']].rename(columns={'key_up_timestamp': 'timestamp'})
-  df_up['action'] = 'up'
-
-  # Combine the DataFrames and sort by timestamp
-  df_events = pd.concat([df_down, df_up], ignore_index=True)
-  df_events = df_events.sort_values(by='timestamp')
-  df_events['time_delta'] = df_events['timestamp'].diff().shift(-1).fillna(0.0)
-  df_events = df_events.drop(columns=['timestamp'])
-  return df_events
-
-
-def play_recorded_keystrokes(recorded_keystrokes_fpath):
+    Returns
+    -------
+    None
+    
+    Notes
+    -----
+    1. key_recorder.py is a script that writes the recording to the recorded_keystrokes_fpath
+        in the correct input format.
+    '''
     df = pd.read_csv(recorded_keystrokes_fpath)
-    #print(df.head())
     df_events = create_event_dataframe(df)
-    print(df_events)
+    if verbosity == 1 or verbosity == 2:
+        print('df_events.head()')
+        print(df_events.head())
+    elif verbosity >= 3:
+        print('df_events')
+        print(df_events)
+    if verbosity == 2 or verbosity == 3:
+        print('df.head()')
+        print(df.head())
+    elif verbosity >= 4:
+        print('df')
+        print(df)
     for _, row in df_events.iterrows():
         key = row['key']
         action = row['action']
@@ -105,10 +135,4 @@ def play_recorded_keystrokes(recorded_keystrokes_fpath):
 
 if __name__ == '__main__':   
     time.sleep(3)
-    #play_recorded_keystrokes('stone_to_gate.csv')
-    #play_recorded_keystrokes('gate_to_outside_cave.csv')
-    #play_recorded_keystrokes('cave_to_top_of_hill.csv')
-    #play_recorded_keystrokes('to_outside_httk.csv')
-    #play_recorded_keystrokes('recorded_keystrokes_altered.csv')
-    #play_recorded_keystrokes('1_end_to_cave.csv')
     play_recorded_keystrokes('recorded_keystrokes.csv')
